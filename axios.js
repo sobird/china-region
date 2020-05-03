a31301b762fe9e4d73b6e9939aa246109891c31b
sobird <i@sobird.me>
2020-05-03 09:57:13 +0800

    town 数据

--- axios.js
+++ axios.js
@@ -5,10 +5,13 @@
  */
 
 const axios = require("axios");
-const iconv = require('iconv-lite');
+const iconv = require("iconv-lite");
+const fs = require("fs");
+const path = require("path");
 
-axios.defaults.headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36';
-axios.defaults.headers['cookie'] = 'AD_RS_COOKIE=20080919; wzws_cid=7ab3c15df715368866d309f45671e082f363f43d5635974fa58de33fbbeeed3fc36016c6d5b258aa65952db4d0998218c04ac3b4ba1039dba050a2a8dbf46b9cc47033d6a2a5ba45bb7cedf84c2ec02d';
+axios.defaults.headers["User-Agent"] =
+  "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36";
+
 axios.defaults.withCredentials = true;
 axios.defaults.timeout = 10000;
 axios.defaults.responseType = "arraybuffer";
@@ -30,6 +33,8 @@ axios.interceptors.request.use(
       request.data = querystring.stringify(request.data);
     }
 
+    request.headers["cookie"] = get_cookie();
+
     return request;
   },
   (error) => {
@@ -40,14 +45,40 @@ axios.interceptors.request.use(
 // 响应拦截器
 axios.interceptors.response.use(
   (response) => {
-    const { config, data } = response;
+    const { config, data, headers } = response;
     data.responseTiming = new Date().getTime() - config.requestTimeStamp;
 
+    set_cookie(headers["set-cookie"]);
     return Promise.resolve(data);
   },
   (error) => {
     return Promise.reject(error);
   }
 );
+
+function set_cookie(cookies = []) {
+  let _cookies = [];
+  cookies.forEach((item) => {
+    let cookie = item.split(";");
+
+    _cookies.push(cookie[0]);
+  });
+
+  if(_cookies.length == 0) {
+      return;
+  }
+
+  _cookies.push('AD_RS_COOKIE=20080917');
+  fs.writeFileSync(path.resolve(__dirname, "./cookie"),  _cookies.join(';'));
+}
+
+function get_cookie() {
+    try{
+        return fs.readFileSync(path.resolve(__dirname, "./cookie"), 'utf-8');
+    } catch {
+        return '';
+    }
+    
+  }
 
 module.exports = axios;
--- index.js
+++ index.js
@@ -10,8 +10,10 @@ const cheerio = require("cheerio");
 const fs = require("fs");
 const path = require("path");
 
-// 城市数据
-getCountyData();
+const puppeteer = require('puppeteer');
+
+// 数据
+getTownData();
 
 // 获取城市数据保存到city.js文件中
 async function getCityData() {
@@ -47,6 +49,7 @@ async function getCityData() {
   writeFileSync("./data/city.js", res);
 }
 
+// 获取县数据
 async function getCountyData() {
   const cities = require("./data/city");
   const pcodes = Object.keys(cities);
@@ -100,10 +103,95 @@ async function getCountyData() {
     }
   }
 
-  console.log(res);
-
   writeFileSync("./data/county.js", res);
 }
+
+// 获取镇数据
+async function getTownData() {
+    const counties = require("./data/county");
+    const city_codes = Object.keys(counties);
+  
+    let res = {};
+  
+    for (let i = 0; i < city_codes.length; i++) {
+      let city_code = city_codes[i];
+      let county_codes = Object.keys(counties[city_code]);
+  
+      for (let i = 0; i < county_codes.length; i++) {
+        let county_code = county_codes[i];
+  
+        let province_code = county_code.slice(0, 2);
+        let city_code = county_code.slice(2, 4);
+        let county_ccode = county_code.slice(0, 6);
+  
+        let path = "/" + province_code + "/" + city_code + "/" + county_ccode + ".html";
+  
+        res[county_code] = {};
+  
+        await sleep(3000);
+  
+        await (async function () {
+          try {
+            console.log(path);
+            const browser = await puppeteer.launch();
+            const page = await browser.newPage();
+            await page.goto(axios.defaults.baseURL + path);
+
+            const data = await page.evaluate((county_code) => {
+                let data = {};
+                let list = [...document.querySelectorAll('.towntable .towntr')];
+                list.forEach(el => {
+                    const t = el.innerText.split('\t');
+                    data[t[0]] = t[1];
+                });
+
+                if(list.length == 0) {
+                    list = [...document.querySelectorAll('.villagetable .villagetr')];
+                    list.forEach(el => {
+                        const t = el.innerText.split('\t');
+                        data[t[0]] = t[1];
+                    });
+                }
+
+                return data;
+              }, county_code);
+
+              res[county_code] = data;
+              
+
+
+            // let html = await axios.get(path);
+            // const $ = cheerio.load(html);
+            // $(".towntable .towntr").each((index, item) => {
+            //   let value = $("td", item).eq(0).text();
+            //   let label = $("td", item).eq(1).text();
+  
+            //   res[county_code][value] = label;
+            // });
+  
+            // let _tmp = Object.keys(res[county_code]);
+            // // 如果获取不到country数据，则抓取town数据
+            // if (_tmp.length == 0) {
+            //   $(".villagetable .villagetr").each((index, item) => {
+            //     let value = $("td", item).eq(0).text();
+            //     let label = $("td", item).eq(1).text();
+  
+            //     res[county_code][value] = label;
+            //   });
+            // }
+  
+            console.log(res[county_code]);
+            await browser.close();
+          } catch (err) {
+            console.log("超时重试...", err);
+            await arguments.callee();
+          }
+        })();
+      }
+    }
+  
+    writeFileSync("./data/town.js", res);
+  }
 
 // 延迟执行
 function sleep(time = 0) {
